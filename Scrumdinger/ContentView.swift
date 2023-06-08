6c771a233434ec62c223ec27da458dde8ddf91fd
Xuexiang Xu <xuexu@microsoft.com>
2023-06-08 21:58:32 +0800

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Xuexiang Xu on 2023/6/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Xuexiang Xu on 2023/6/8.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
